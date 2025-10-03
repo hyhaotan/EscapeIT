9f12e60498c3f91f3e3491f3278978fe69748015
hy hao tan <hyhaotan@gmail.com>
2025-10-03 19:53:31 +0700

    new beginning

---
+++ EscapeIT/EscapeIT.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EscapeIT.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EscapeIT, "EscapeIT" );
+
+DEFINE_LOG_CATEGORY(LogEscapeIT)
---
+++ EscapeIT/EscapeITGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EscapeITGameMode.h"
+
+AEscapeITGameMode::AEscapeITGameMode()
+{
+	// stub
+}
